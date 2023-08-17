a8e6b44a8a507c77850b1979c0408b9672c14ee3
aryaratan <31pragyaratan@gmail.com>
2023-08-17 19:06:22 +0530

    Changed Database connection

--- config/index.js
+++ config/index.js
@@ -4,13 +4,19 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 
-mongoose.connect(process.env.DATABASE, {useNewUrlParser:true});
-
-
-const db = mongoose.connection;
-
-db.on('error',console.error.bind(console,"Error"));
-
-db.once('open',function(){
-    console.log("Successfully connected to DB");
+mongoose.connect(process.env.DATABASE, {useNewUrlParser:true})
+.then(()=>{
+    console.log('connected to Database');
+})
+.catch((err)=>{
+    console.log('Connection failed', err);
 });
+
+
+// const db = mongoose.connection;
+
+// db.on('error',console.error.bind(console,"Error"));
+
+// db.once('open',function(){
+//     console.log("Successfully connected to DB");
+// });
