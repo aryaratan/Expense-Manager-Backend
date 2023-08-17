2c6b0a13050b35e23aec30a0ccc28a2355d76c6a
aryaratan <31pragyaratan@gmail.com>
2023-08-17 13:55:33 +0530

    Changed the connectiom with mongoose

--- config/index.js
+++ config/index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 
-mongoose.connect(process.env.DATABASE);
+mongoose.connect(process.env.DATABASE, {useNewUrlParser:true});
 
 
 const db = mongoose.connection;
